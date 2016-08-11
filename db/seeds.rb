b7a76899c35d168fff0a3c7271fa5b7e7255586b
Constance Toulemonde <toulemonde.const@gmail.com>
2016-08-11 12:22:49 +0200

    Models and seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+Ingredient.create(name: "armagnac")
+Ingredient.create(name: "sucre de canne")
+Ingredient.create(name: "sirop de framboise")
+Ingredient.create(name: "egg")
+Ingredient.create(name: "miel")
+Ingredient.create(name: "vanilla")
