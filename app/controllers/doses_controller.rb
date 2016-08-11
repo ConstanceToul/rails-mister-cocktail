3c82acc88b7b8450468afc682793cf2a1f5553e0
Constance Toulemonde <toulemonde.const@gmail.com>
2016-08-11 15:54:26 +0200

    end of front

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,21 +1,39 @@
 class DosesController < ApplicationController
 
+before_action :set_cocktail, only: [:new, :create]
+
   def new
-    @cocktail = Cocktail.find(params[:cocktail_id])
     @dose = Dose.new
   end
 
   def create
-    @cocktail = Cocktail.find(params[:cocktail_id])
     @dose = Dose.new(dose_params)
-    @dose.save
+    @dose.cocktail = @cocktail
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @dose = Dose.find(params[:id])
+    @cocktail = Cocktail.find(@dose.cocktail_id)
+    @dose.destroy
     redirect_to cocktail_path(@cocktail)
   end
 
 private
 
-  def dose_params
-    params.require(:dose).permit(:description)
+  def set_cocktail
+    @cocktail = Cocktail.find(params[:cocktail_id])
   end
 
+
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id)
+  end
+
+
 end
+
